5cfac570d03d1220ee2159eb45a7d62a390c861a
mv9k <mv9kjw@gmail.com>
2016-03-10 10:31:04 -0700

    write to read-only file

--- March10/fileSystem.js
+++ March10/fileSystem.js
@@ -29,6 +29,59 @@ fs.appendFile('March10/message.txt', '\nThis is the appended line!', ()=>{
     console.log('appended to message.txt!');
 });
 
+fs.readFile('March10/data.json', 'utf8', (errors, jsonFile)=>{
+    var transferData = JSON.parse(jsonFile);
+    fs.appendFile('March10/message.txt', '\nName of user: '+transferData.name+ '\nMessage of user: '+transferData.message);
+});
+
+// Creates a file
+
+//fs.writeFile(
+//    'March10/readOnly.txt',
+//    'Can\'t change me!',
+//    {
+//        encoding: 'utf8',
+//        flag: 'w'
+//
+//    },
+//    ()=>{console.log('created a file!')}
+//);
+
+// Sets file to read-only
+
+//fs.chmod(
+//    'March10/readOnly.txt',
+//    '0o44',
+//    ()=>{console.log('set file to read-only');}
+//);
+
+
+
+// read-only file
+
+fs.chmod(
+    'March10/readOnly.txt',
+    0o444,
+    ()=>{
+        fs.writeFile('March10/readOnly.txt', 'Add something to this read-only file!', (error)=>{
+            console.log('attempted to append to read-only file!');
+            console.log(error);
+
+            if(error) {
+                fs.chmod(
+                    'March10/readOnly.txt',
+                    0o666,
+                    ()=>{
+                        fs.appendFile('March10/readOnly.txt', '\nAdded content', (error)=>{
+                            console.log(error);
+                            console.log('added content to file');
+                        })
+                    }
+                )
+            }
+        });
+    }
+);
 
 
 
