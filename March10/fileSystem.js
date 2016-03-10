be0e9a4e0277eee56283cbe34c47634777093087
mv9k <mv9kjw@gmail.com>
2016-03-10 09:10:12 -0700

    create new file and then append new lines to it.

--- March10/fileSystem.js
+++ March10/fileSystem.js
@@ -6,6 +6,14 @@ var fs = require('fs');
 
 var read = fs.readFile('March10/data.json', 'utf8', (errors, jsonFile)=>{
     console.log(jsonFile);
+
+    var fileData = JSON.parse(jsonFile);
+    console.log(fileData.name, ':');
+    console.log('"', fileData.message, '"');
+});
+
+var write = fs.writeFile('March10/message.txt', 'new message!', 'utf8', ()=>{
+    console.log('writing new file!');
 });
 
 fs.stat('March10/data.json', (error, stats) =>{
@@ -16,6 +24,12 @@ fs.stat('March10/data.json', (error, stats) =>{
         console.log('file doesn\'t exist');
     }
 });
+
+fs.appendFile('March10/message.txt', '\nThis is the appended line!', ()=>{
+    console.log('appended to message.txt!');
+});
+
+
 
 
 
